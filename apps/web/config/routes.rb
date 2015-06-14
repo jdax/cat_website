26b51fbf9c33b3b2afe3fbd010ef77d6332bbab5
Nikki Murray <nikki.liz.murray@gmail.com>
2015-06-13 23:20:33 -0400

    adding routes

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -6,3 +6,7 @@
 # This route will look for `Web::Controllers::Home::Index` action in `apps/web/controllers/home/index.rb`.
 # Please, uncomment the following line to have a working example.
  get '/', to: 'home#index'
+ get '/cats', to: 'cats#index'
+ get '/cats/didi', to: 'cats#didi'
+ get '/cats/tomwaits', to: 'cats#tomwaits'
+ get 'cats/stormy', to: 'cats#stormy'
